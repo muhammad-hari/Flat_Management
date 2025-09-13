4c2303ff579b6ae771eaa736e44f6e2838ef9570
Ary Raja Parulian <Iqbal.Maulana@contracted.sampoerna.com>
2025-09-14 00:07:15 +0700

    changes

--- src/MyApp.Infrastructure/obj/Debug/net8.0/MyApp.Infrastructure.AssemblyInfo.cs
+++ src/MyApp.Infrastructure/obj/Debug/net8.0/MyApp.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApp.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+646e3a221f2e4d92058b070e0011aca8af6848ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApp.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApp.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
